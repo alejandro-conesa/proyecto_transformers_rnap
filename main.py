7db45686162ed6fc1ec188734310f87c4ef191db
ablindaloe <garreveraalex@gmail.com>
2026-05-07 13:19:51 +0200

    main.py inicial creado

---
+++ main.py
@@ -0,0 +1,25 @@
+from datos import IMDBDataModule
+from modelo import IMDBModel
+import lightning as L
+from lightning.pytorch.callbacks import EarlyStopping, ModelCheckpoint, ModelSummary
+
+EPOCHS = 10
+NAME   = "mini_bert"
+
+
+dm    = IMDBDataModule(batch_size=64, max_len=256)
+model = IMDBModel(vocab_size=10_000, num_layers=4, hidden_dim=256)
+
+early_stopping = EarlyStopping(monitor="val_loss", patience=3, mode="min")
+ckpt = ModelCheckpoint(
+    dirpath="weights",
+    filename=f"{EPOCHS}ep-{NAME}",
+    verbose=True,
+    monitor="val_character_error_rate", 
+    mode="min"
+)
+summary = ModelSummary(max_depth=3)
+
+trainer = L.Trainer(max_epochs=10, accelerator="auto", callbacks=[early_stopping, ckpt, summary])
+trainer.fit(model, dm)
+trainer.test(model, dm)
